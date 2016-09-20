2d0457c479a84b933fba455310d0d212799eab0c
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2016-09-20 20:25:44 +0300

    added lists for dev, test and deploy

--- kanban-board/src/app/board/board.component.ts
+++ kanban-board/src/app/board/board.component.ts
@@ -7,9 +7,29 @@ import { Task } from '../domain/task';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
-  freshTasks: Array<Task>;
+  newTasks: Array<Task>;
+  developmentTasks: Array<Task>;
+  testTasks: Array<Task>;
+  deployTasks: Array<Task>;
+
   constructor() {
-    this.freshTasks = [
+    this.newTasks = [
+                          new Task(1,'tasl1', 'task1 description'),
+                          new Task(2,'tasl2', 'task2 description'),
+                          new Task(3,'tasl3', 'task3 description')
+                      ];
+
+    this.developmentTasks = [
+                          new Task(1,'tasl1', 'task1 description'),
+                          new Task(2,'tasl2', 'task2 description'),
+                          new Task(3,'tasl3', 'task3 description')
+                      ];
+   this.testTasks = [
+                          new Task(1,'tasl1', 'task1 description'),
+                          new Task(2,'tasl2', 'task2 description'),
+                          new Task(3,'tasl3', 'task3 description')
+                      ];
+   this.deployTasks = [
                           new Task(1,'tasl1', 'task1 description'),
                           new Task(2,'tasl2', 'task2 description'),
                           new Task(3,'tasl3', 'task3 description')
