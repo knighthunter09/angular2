1d24f67e5b0c5cac1632e30a87bfbf5ea1c8effb
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2016-09-23 19:02:18 +0300

    added back-end project

---
+++ kanbanboardbe/src/main/java/com/evgeni/kanbanboardbe/KanbanboardbeApplication.java
@@ -0,0 +1,12 @@
+package com.evgeni.kanbanboardbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanboardbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanboardbeApplication.class, args);
+	}
+}
