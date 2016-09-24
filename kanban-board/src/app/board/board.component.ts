43aa5b28204a05f8b44cebaec6724611b4c39030
Evgeni Dimitrov <evgeny.d.dimitrov@gmail.com>
2016-09-24 17:40:23 +0300

    added http - status filter pipe not working

--- kanban-board/src/app/board/board.component.ts
+++ kanban-board/src/app/board/board.component.ts
@@ -2,17 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { Task } from '../task/task';
 import { TaskStatus } from '../task/task-status';
 import { TaskService } from '../task/task.service';
+
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
-  tasks: Array<Task>;
+  tasks: Observable<Task>;
   taskFormOpen: boolean = false;
   public TaskStatus = TaskStatus;
   constructor(private taskService:TaskService) {
-    this.tasks = taskService.getTasks();
+    taskService.getTasks()
+    .map(res => res.json())
+    .subscribe(t => {console.log("sub:");console.log(t._embedded.tasks); this.tasks = t._embedded.tasks }, 
+                err => console.error("Error constructor "+err), 
+                () => console.log('done'));
   }
 
   ngOnInit() {
@@ -23,8 +29,17 @@ export class BoardComponent implements OnInit {
   }
 
   taskAddedHandler(task: Task) {
-    this.taskService.addNewTask(task);
-    this.tasks = Array.from(this.taskService.getTasks());//replace the whole array, so the ngFor with a pipe can be uodated
+    this.taskService.addNewTask(task)
+              .map(res => res.json)
+                .subscribe(t => console.log(t), 
+                err => console.error("Error add "+err), 
+                () => console.log('done'));;
+    //this.tasks = Array.from(this.taskService.getTasks());//replace the whole array, so the ngFor with a pipe can be uodated
+    this.taskService.getTasks()
+    .map(res => res.json())
+    .subscribe(t => {console.log("sub:");console.log(t); this.tasks = t}, 
+                err => console.error("Error get "+err), 
+                () => console.log('done'));
     this.taskFormOpen = false;
   }
 }
--- kanban-board/src/app/task/task.service.ts
+++ kanban-board/src/app/task/task.service.ts
@@ -1,26 +1,16 @@
 import { Task } from './task';
 import { TaskStatus } from './task-status';
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Injectable()
 export class TaskService {
-    private tasks:Array<Task>;
-    constructor(){
-        this.tasks = [
-                          new Task(1,'tasl1', 'task1 description', TaskStatus.New),
-                          new Task(2,'tasl2', 'task2 description', TaskStatus.New),
-                          new Task(3,'tasl3', 'task3 description', TaskStatus.New),
-                          new Task(4,'tasl4', 'task4 description', TaskStatus.Dev),
-                          new Task(5,'tasl2', 'task2 description', TaskStatus.Dev),
-                          new Task(6,'tasl6', 'task6 description', TaskStatus.Test),
-                          new Task(7,'tasl7', 'task7 description', TaskStatus.Deploy)
-                      ];
+    constructor(private http: Http){
     }
     getTasks() {
-        return this.tasks;
+        return this.http.get('http://localhost:8080/tasks');
     }
     addNewTask(task: Task){
-        this.tasks.push(task);
-        //this.tasks = this.tasks.concat(task);
+        return this.http.post('http://localhost:8080/tasks',task);
     }
 }
